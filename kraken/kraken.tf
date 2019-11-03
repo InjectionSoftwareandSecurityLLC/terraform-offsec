4ed0424cfd204262de30be5ea3e01e7961d4738b
3ndG4me <3ndG4me@protonmail.com>
2019-11-02 21:40:32 -0400

    90 percent done with kraken automation, it's alive...

--- kraken/kraken.tf
+++ kraken/kraken.tf
@@ -14,6 +14,12 @@ data "aws_ami" "ubuntu" {
 
 }
 
+# Retrieve the AZ where we want to create network resources
+# This must be in the region selected on the AWS provider.
+data "aws_availability_zone" "kraken_zone" {
+  name = "us-east-2a"
+}
+
 resource "aws_security_group" "kraken_ssh" {
   name        = "kraken_ssh_group"
   description = "Allow Port Kraken SSH access"
@@ -23,7 +29,7 @@ resource "aws_security_group" "kraken_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks  = ["YOUR_IP"]
+    cidr_blocks  = ["75.62.72.13/32"]
   }
 
   egress {
@@ -39,15 +45,27 @@ resource "aws_instance" "kraken" {
   instance_type = "p2.8xlarge"
   security_groups = ["${aws_security_group.kraken_ssh.name}"]
   key_name = "primary-kraken-key"
-  user_data = "${file("./kraken_setup.sh")}"
-  
+  availability_zone = "${data.aws_availability_zone.kraken_zone.name}"
+
+# Run this script as root once provisioning is complete. Ubuntu won't fetch our dependencies correctly if we run a remote-exec so this can't be fully automated for now.
+ provisioner "file" {
+    source      = "kraken_setup.sh"
+    destination = "/tmp/kraken_setup.sh"
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = "${file("~/.ssh/primary-kraken-key.pem")}"
+      host        = "${self.public_ip}"
+    }
+  }
+
   tags = {
     Name = "Kraken"
   }
 }
 
 resource "aws_ebs_volume" "wordlists" {
-  availability_zone = "us-east-2a"
+  availability_zone = "${data.aws_availability_zone.kraken_zone.name}"
   size              = 500
 
   tags = {
@@ -56,10 +74,30 @@ resource "aws_ebs_volume" "wordlists" {
 }
 
 resource "aws_volume_attachment" "wordlists_volume" {
-  device_name = "/dev/sdh"
+  device_name = "/dev/xvdh"
   volume_id   = "${aws_ebs_volume.wordlists.id}"
   instance_id = "${aws_instance.kraken.id}"
 }
+
+resource "null_resource" "data_config" {
+  depends_on = ["aws_volume_attachment.wordlists_volume", "aws_instance.kraken"]
+  
+  ## run unmount commands when destroying the data volume
+  provisioner "remote-exec" {
+    when = "destroy"
+    on_failure = "continue"
+    connection {
+      type = "ssh"
+      agent = false
+      timeout = "60s"
+      user = "ubuntu"
+      private_key = "${file("~/.ssh/primary-kraken-key.pem")}"
+      host = "${aws_instance.kraken.public_ip}"
+    }
+    inline = ["sudo umount /media/wordlists"]
+  }
+}
+
 
 output "IP" {
     value = "${aws_instance.kraken.public_ip}"
--- kraken/kraken_setup.sh
+++ kraken/kraken_setup.sh
@@ -1,6 +1,15 @@
-sudo apt update && sudo apt dist-upgrade -y
-sudo apt install hashcat
-sudo apt john
-wget "http://us.download.nvidia.com/tesla/418.87/nvidia-driver-local-repo-ubuntu1804-418.87.01_1.0-1_amd64.deb"
-sudo dpkg -i nvidia-driver-local-repo-ubuntu1804-418.87.01_1.0-1_amd64.deb
-sudo reboot
+sudo mkfs.ext4 /dev/xvdh
+sudo mkdir /media/wordlists
+sudo mount /dev/xvdh /media/wordlists
+echo "Here I would download my wordlists..."
+sudo apt update -y
+sudo apt install hashcat -y
+sudo apt install john -y
+sudo apt install make -y
+sudo apt install gcc -y
+sudo apt install pkg-config -y
+VERSION=410.104
+wget "http://us.download.nvidia.com/tesla/${VERSION}/NVIDIA-Linux-x86_64-$VERSION.run" -O /tmp/nvidia_install.run
+chmod +x /tmp/nvidia_install.run
+sudo /tmp/nvidia_install.run -s
+rm /tmp/nvidia_install.run 
